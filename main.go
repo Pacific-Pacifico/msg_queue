71c0b38f1365ddb7947ad58f442553aeab924e13
prashant kumar <pacific@codewire>
2021-07-26 13:13:47 +0530

    combined with transcoding process

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+var (
+	amqpConn *amqp.Connection
+	ch       *amqp.Channel
+	err      error
+)
+
+func LogIfError(fatal bool, err error, msg string) {
+	if err != nil {
+		if fatal {
+			log.Fatalf("Error: %s \nmsg: %s", err, msg)
+		} else {
+			log.Printf("Error: %s \nmsg: %s", err, msg)
+		}
+	}
+}
+
+func getAmqpConnection() {
+	rmq_env := os.Getenv("RMQ_ENV")
+	amqpConn, err = amqp.Dial(rmq_env)
+	LogIfError(true, err, "Error connecting to RabbitMQ instance")
+	// defer amqpConn.Close()
+	fmt.Println("connected to rabbitmq instance")
+}
+
+func OpenChannel() {
+	ch, err = amqpConn.Channel()
+	LogIfError(true, err, "Failed to open a channel")
+	// defer ch.Close()
+}
+
+func main() {
+	getAmqpConnection()
+	OpenChannel()
+	addToQueue("sample.mp4")
+}
--- producer.go
+++ producer.go
@@ -2,38 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"time"
 
 	"github.com/streadway/amqp"
 )
 
-func LogIfError(fatal bool, err error, msg string) {
-	if err != nil {
-		if fatal {
-			log.Fatalf("Error: %s \nmsg: %s", err, msg)
-		} else {
-			log.Printf("Error: %s \nmsg: %s", err, msg)
-		}
-	}
-}
-
-func main() {
-	rmq_env := os.Getenv("RMQ_ENV")
-	conn, err := amqp.Dial(rmq_env)
-	LogIfError(true, err, "Error connecting to RabbitMQ instance")
-	defer conn.Close()
-
-	fmt.Println("connected to rabbitmq instance")
-
-	ch, err := conn.Channel()
-	LogIfError(true, err, "Failed to open a channel")
-	defer ch.Close()
-
+func addToQueue(fileName string) {
 	q, err := ch.QueueDeclare(
-		"TaskQueue",
+		"TranscodeQueue",
 		false,
 		false,
 		false,
@@ -43,19 +18,16 @@ func main() {
 	LogIfError(true, err, "Failed to declare Queue")
 	fmt.Println(q)
 
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	str := fmt.Sprintf("%s %d", "Hello world", r1.Intn(100))
 	err = ch.Publish(
 		"",
-		"TaskQueue",
+		"TranscodeQueue",
 		false,
 		false,
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        []byte(str),
+			Body:        []byte(fileName),
 		},
 	)
 	LogIfError(true, err, "Failed to publish to work queue")
-	fmt.Println("Message published:", str)
+	fmt.Println("Message published:", fileName)
 }
